5cd2cc5a085ac286a35ddbae43995a26c3be8455
Jon White <jonwhite@jons-imac.home>
2020-11-18 11:58:54 +0000

    update plugins and worked on styling and content

---
+++ wp-content/themes/understrap-child/blocks/block-section-title-text.php
@@ -0,0 +1,13 @@
+<section id="about" class="section <?php block_field( 'section-class' ); ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+				<h2 class="<?php block_field( 'section-title-class' ); ?>"><?php block_field( 'section-title' ); ?></h2>
+            </div>
+            <div class="col-md-8">
+				<?php block_field( 'section-text-lead' ); ?>
+				<?php block_field( 'section-text' ); ?>
+            </div>
+        </div>
+    </div>
+</section>
